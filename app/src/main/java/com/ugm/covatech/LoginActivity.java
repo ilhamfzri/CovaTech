f3bd0bc7722877facf7b8e156a26aed3bf00abe7
Ilham Fazri <ilhamfazri3rd@gmail.com>
2021-06-30 00:53:39 +0700

    add feature for login & register

--- app/src/main/java/com/ugm/covatech/LoginActivity.java
+++ app/src/main/java/com/ugm/covatech/LoginActivity.java
@@ -1,14 +1,81 @@
 package com.ugm.covatech;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText mEmail,mPassword;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mEmail = findViewById(R.id.editEmail);
+        mPassword = findViewById(R.id.editPassword);
+
+        fAuth=FirebaseAuth.getInstance();
+
     }
+
+    public void setRegister(View view) {
+        Intent nextActivity = new Intent(this, Register.class);
+//        nextActivity.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(nextActivity, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+//        overridePendingTransition(0,0);
+    }
+
+    public void setLogin(View view){
+        String vEmail = mEmail.getText().toString();
+        String vPassword = mPassword.getText().toString();
+        if(TextUtils.isEmpty(vPassword)) {
+            mPassword.setError("Password wajib diisi!");
+            return;
+        }
+        if(TextUtils.isEmpty(vEmail)) {
+            mEmail.setError("Email wajib diisi!");
+            return;
+        }
+
+        fAuth.signInWithEmailAndPassword(vEmail, vPassword).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser user = fAuth.getCurrentUser();
+                    if(!user.isEmailVerified()){
+                        fAuth.signOut();
+                        Toast.makeText(LoginActivity.this, "Cek Email dan Mohon Verifikasi Akun Anda!", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Intent mainActivity = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(mainActivity, ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this).toBundle());
+                    }
+                }
+                else{
+                    mEmail.setError("Email atau password anda salah!");
+                    mPassword.setError("EEmail atau password anda salah!");
+                    Toast.makeText(LoginActivity.this, "Login Gagal, Email atau Password Salah!!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
+
+    }
+
 }
---
+++ app/src/main/java/com/ugm/covatech/Register.java
@@ -0,0 +1,131 @@
+package com.ugm.covatech;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Register extends AppCompatActivity {
+    EditText mName, mEmail, mPassword1, mPassword2;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore=FirebaseFirestore.getInstance();
+
+        mName = findViewById(R.id.editName);
+        mEmail = findViewById(R.id.editEmail);
+        mPassword1 = findViewById(R.id.editPassword);
+        mPassword2 = findViewById(R.id.editPassword2);
+    }
+
+    public static boolean isValid(String email)
+    {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
+    }
+
+    public void setRegister(View view){
+        final String vEmail = mEmail.getText().toString().trim();
+        final String vName = mName.getText().toString().trim();
+        final String vPassword= mPassword1.getText().toString().trim();
+        final String vPassword2 = mPassword2.getText().toString().trim();
+
+        //Check Email Validity
+        if(isValid(vEmail)==false){
+            mEmail.setError("Masukan email yang benar!");
+
+            return;
+        }
+
+        //Check Name
+        if(TextUtils.isEmpty(vName)){
+            mName.setError("Masukan Nama anda!");
+            return;
+        }
+
+        //Check Password length, if below 6 character return error
+        if(vPassword.length()<6 || vPassword2.length()<6){
+            mPassword1.setError("Password minimal 6 karakter!");
+            mPassword2.setError("Password minimal 6 karakter!");
+            return;
+        }
+        //Check Password
+        if(vPassword.equals(vPassword2)==false){
+            mPassword1.setError("Password anda berbeda!");
+            mPassword2.setError("Password anda berbeda!");
+            return;
+        }
+
+        fAuth.createUserWithEmailAndPassword(vEmail, vPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser user = fAuth.getCurrentUser();
+                    user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Toast.makeText(Register.this, "Email Verifikasi Telah Dikirim!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+                String userID = fAuth.getCurrentUser().getUid();
+                DocumentReference documentReference = fStore.collection("users").document(userID);
+
+                // Save user data to firestore
+                Map<String, Object> userData = new HashMap<>();
+                userData.put("Name", vName);
+                userData.put("Email", vEmail);
+                documentReference.set(userData).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        fAuth.signOut();
+                        Intent loginActivity = new Intent(Register.this, LoginActivity.class);
+                        startActivity(loginActivity, ActivityOptions.makeSceneTransitionAnimation(Register.this).toBundle());
+                    }
+                });
+            }
+
+
+
+        });
+    }
+
+    public void setLogin(View view){
+        Intent loginActivity = new Intent(Register.this, LoginActivity.class);
+        startActivity(loginActivity, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+    }
+}
