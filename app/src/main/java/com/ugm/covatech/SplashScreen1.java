b9cf64195760a52450f49053a34c4d1774387521
Ilham Fazri <ilhamfazri3rd@gmail.com>
2021-07-01 02:16:15 +0700

    add main activity menu & bottom navigation bar

--- app/src/main/java/com/ugm/covatech/SplashScreen1.java
+++ app/src/main/java/com/ugm/covatech/SplashScreen1.java
@@ -1,10 +1,12 @@
 package com.ugm.covatech;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -14,17 +16,25 @@ import android.view.View;
 
 public class SplashScreen1 extends AppCompatActivity {
     Handler handler;
+    FirebaseAuth fAuth;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen1);
 
+        fAuth=FirebaseAuth.getInstance();
         handler=new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(SplashScreen1.this, SplashScreen2.class);
+                if (fAuth.getCurrentUser() != null) {
+                    intent= new Intent(SplashScreen1.this, MainActivity.class);
+                }
+                else{
+                    intent= new Intent(SplashScreen1.this, SplashScreen2.class);
+                }
                 startActivity(intent);
                 finish();
             }
