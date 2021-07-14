e15a17186e51fe589e6c64d81c774db7f8abc3ab
Ilham Fazri <ilhamfazri3rd@gmail.com>
2021-07-15 02:44:51 +0700

    integration adapter, bottom sheet and add feature dial and direction

--- app/src/main/java/com/ugm/covatech/FasilitasKesehatan.java
+++ app/src/main/java/com/ugm/covatech/FasilitasKesehatan.java
@@ -4,66 +4,123 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
 public class FasilitasKesehatan extends AppCompatActivity {
-    ListView listView;
+    final ArrayList<String> arrayName = new ArrayList<String>();
+    final ArrayList<String> arrayAddress = new ArrayList<String>();
+    final ArrayList<String> arrayDistance = new ArrayList<String>();
+    final ArrayList<String> arrayContact = new ArrayList<String>();
+    final ArrayList<String> arrayDirection = new ArrayList<String>();
+    RecyclerView recyclerView;
+    AdapterFasilitasKesehatan adapterFasilitasKesehatan;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fasilitas_kesehatan);
+        db = FirebaseFirestore.getInstance();
+        recyclerView = findViewById(R.id.recyclerView);
+        loadData();
 
-        listView = findViewById(R.id.listView);
-        String mLocation[] = {"Rumah Sakit Bayangkara", "Rumah Sakit UGM"};
-        String mAddress[] = {"Jarak 4km", "Jarak 5km"};
-
-        MyAdapter adapter = new MyAdapter(this, mLocation, mAddress);
-        listView.setAdapter(adapter);
     }
 
-    class MyAdapter extends ArrayAdapter<String> {
+    public void loadData(){
+        db.collection("fasilitas_kesehatan").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    for(QueryDocumentSnapshot document : task.getResult()){
+                        arrayName.add(document.getString("Name"));
+                        arrayAddress.add(document.getString("Address"));
+                        arrayContact.add(document.getString("Contact"));
+                        arrayDirection.add(document.getString("Direction"));
 
-        Context context;
-        String rLocation[];
-        String rAddress[];
+                    }
 
-        MyAdapter(Context c, String location[], String address[]){
-            super(c, R.layout.row_fasilitas_kesehatan, location);
-            this.context = c;
-            this.rLocation = location;
-            this.rAddress = address;
-        }
+                    final String [] stringArrayName = arrayName.toArray(new String[0]);
+                    final String [] stringArrayDistance = arrayAddress.toArray(new String[0]);
+                    final String [] stringArrayAddress = arrayAddress.toArray(new String[0]);
+                    final String [] stringArrayContact = arrayContact.toArray(new String[0]);
+                    final String [] stringArrayDirection = arrayDirection.toArray(new String[0]);
 
-        @NonNull
-        @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
-            LayoutInflater layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View row = layoutInflater.inflate(R.layout.row_fasilitas_kesehatan, parent, false);
-            TextView location = row.findViewById(R.id.location);
-            TextView distance = row.findViewById(R.id.distance);
+                    adapterFasilitasKesehatan =new AdapterFasilitasKesehatan(FasilitasKesehatan.this, stringArrayName, stringArrayDistance, new AdapterFasilitasKesehatan.ClickListener() {
+                        @Override
+                        public void onPositionClicked(int position) {
+                            showBottomSheet(stringArrayName[position],stringArrayAddress[position], 3, stringArrayContact[position], stringArrayDirection[position]);
+                        }
+                    });
+                    recyclerView.setAdapter(adapterFasilitasKesehatan);
+                    recyclerView.setLayoutManager(new LinearLayoutManager(FasilitasKesehatan.this));
+                }
+            }
+        });
+    }
 
-            Typeface poppinsSemiBold = ResourcesCompat.getFont(context, R.font.poppins_semibold);
-            Typeface poppins = ResourcesCompat.getFont(context, R.font.poppins);
+    public void showBottomSheet(String sName, String sAddress, Integer sDistance, final String sContact, final String sDirection){
+        TextView namePlace, addressPlace;
+        Button buttonDial;
 
-            location.setTypeface(poppinsSemiBold);
-            location.setText(rLocation[position]);
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(FasilitasKesehatan.this, R.style.BottomSheetDialogTheme);
+        View bottomSheetView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.layout_bottom_sheet_fasilitas_kesehatan, (LinearLayout)findViewById(R.id.bottomSheetContainer));
 
-            distance.setTypeface(poppins);
-            distance.setText(rAddress[position]);
+        bottomSheetView.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+                String number=sContact.replaceAll("[\\D]", "");
+                dialIntent.setData(Uri.parse("tel:"+number));
+                startActivity(dialIntent);
+            }
+        });
+
+        bottomSheetView.findViewById(R.id.directionPlaces).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("google.navigation:q="+sDirection);
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
+
+        namePlace = bottomSheetView.findViewById(R.id.namePlace);
+        namePlace.setText(sName);
+        addressPlace = bottomSheetView.findViewById(R.id.addressPlace);
+        addressPlace.setText(sAddress);
+        buttonDial = bottomSheetView.findViewById(R.id.button);
+        buttonDial.setText(sContact);
 
 
-            return row;
-        }
+        bottomSheetDialog.setContentView(bottomSheetView);
+        bottomSheetDialog.show();
+
     }
 }
