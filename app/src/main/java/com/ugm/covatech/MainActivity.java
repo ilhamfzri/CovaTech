ffb2b015954006641d4a3e529610013974d88af2
Ilham Fazri <ilhamfazri3rd@gmail.com>
2021-07-16 02:46:58 +0700

    add feature to show current "locality" and "admin area"

--- app/src/main/java/com/ugm/covatech/MainActivity.java
+++ app/src/main/java/com/ugm/covatech/MainActivity.java
@@ -1,26 +1,54 @@
 package com.ugm.covatech;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.ActivityOptions;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    TextView currentLocation;
+    FusedLocationProviderClient mFusedLocationClient;
+    int PERMISSION_ID = 44;
+    double currentLatitude;
+    double currentLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
     }
 
     public void setFasilitas(View view){
@@ -29,5 +57,89 @@ public class MainActivity extends AppCompatActivity {
         startActivity(nextActivity, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
 //        overridePendingTransition(0,0);
     }
-    
+
+    @SuppressLint("MissingPermission")
+    private void getLastLocation() {
+        Log.d("Test","test");
+        // check if permissions are given
+        if (checkPermissions()) {
+
+            // check if location is enabled
+            if (isLocationEnabled()) {
+                mFusedLocationClient.getLastLocation()
+                        .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                // Got last known location. In some rare situations this can be null.
+                                if (location != null) {
+                                    currentLatitude = location.getLatitude();
+                                    currentLongitude = location.getLongitude();
+                                    Log.d("Current Location", "Latitude :" + Double.toString(currentLatitude) +", Longitude : "+ Double.toString(currentLongitude));
+                                    Geocoder geoCoder = new Geocoder(MainActivity.this);
+                                    try {
+                                        List<Address> address = geoCoder.getFromLocation(currentLatitude, currentLongitude, 5);
+                                        Log.d("Country", address.get(0).getCountryCode());
+                                        Log.d("Localy", address.get(0).getLocality());
+                                        Log.d("Feature Name", address.get(0).getFeatureName());
+                                        Log.d("Admin Area", address.get(0).getAdminArea());
+
+                                        currentLocation = findViewById(R.id.currentLocation);
+                                        currentLocation.setText(address.get(0).getLocality()+", "+address.get(0).getAdminArea());
+
+                                    } catch (IOException e) {}
+                                    catch (NullPointerException e) {}
+                                }
+                            }
+                        });
+            }
+            else {
+                Toast.makeText(this, "Please turn on" + " your location...", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        } else {
+            // if permissions aren't available,
+            // request for permissions
+            requestPermissions();
+        }
+    }
+
+    // method to check for permissions
+    private boolean checkPermissions() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+        // If we want background location
+        // on Android 10.0 and higher,
+        // use:
+        // ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    // method to request for permissions
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ID);
+    }
+
+    // method to check
+    // if location is enabled
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    // If everything is alright then
+    @Override
+    public void
+    onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            }
+        }
+    }
+
+
 }
