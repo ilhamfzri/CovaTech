3c7e6ea747f41812b3d01964d2cfa2be74e0e566
Ilham Fazri <ilhamfazri3rd@gmail.com>
2021-07-26 02:43:56 +0700

    integrated covatribute with main menu

--- app/src/main/java/com/ugm/covatech/MainActivity.java
+++ app/src/main/java/com/ugm/covatech/MainActivity.java
@@ -54,6 +54,12 @@ public class MainActivity extends AppCompatActivity {
         getLastLocation();
     }
 
+    public void setCovaTribute(View view){
+        Intent nextActivity =  new Intent(MainActivity.this, CovaTributeMain.class);
+        nextActivity.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(nextActivity);
+    }
+
     public void setFasilitas(View view){
         Intent nextActivity =  new Intent(MainActivity.this, FasilitasKesehatan.class);
         nextActivity.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
